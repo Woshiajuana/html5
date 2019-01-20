8dab6be2050016220a27e8d7f56444a84eea2e2e
Woshiajuana <zhigang.chen@owulia.com>
2019-01-20 19:38:53 +0800

    提交更新页面数据

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,5 +1,38 @@
-// import $                from 'jquery'
+import $                from 'jquery'
 // import Toast            from 'utils/toast.util'
+
+
+const Animation = {
+    $el: $('.cards'),
+    index: -1,
+    number: 0,
+    library: ['flutter1', 'flutter2', 'flutter3', 'flutter4'],
+    libraryIndex: -1,
+    libraryNumber: 0,
+    init () {
+        this.number = this.$el.length;
+        this.libraryNumber = this.library.length;
+    },
+    start () {
+        this.init();
+        this.run();
+    },
+    run () {
+        this.index++;
+        this.libraryIndex++;
+        if (this.index >= this.number)
+            this.index = 0;
+        if (this.libraryIndex >= this.libraryNumber)
+            this.libraryIndex = 0;
+        this.$el.eq(this.index).addClass(this.library[this.libraryIndex]);
+        ((index, libraryIndex) => {
+            setTimeout(() => {
+                this.$el.eq(index).removeClass(this.library[libraryIndex]);
+            }, 6000);
+        })(this.index, this.libraryIndex);
+        setTimeout(this.run.bind(this), 2000);
+    },
+};
 
 // 控制器
 const Controller = {
@@ -8,6 +41,7 @@ const Controller = {
             direction: 'vertical'
         });
         this.getElById('next-button').addEventListener('click', this.handleNextSlide.bind(this))
+        Animation.start();
     },
     handleNextSlide () {
         this.swiper.slideNext();
@@ -17,3 +51,4 @@ const Controller = {
     }
 };
 Controller.init();
+
