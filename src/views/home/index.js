aa5b8464a22d37c6a5318908a578e0ab0d1ac743
Woshiajuana <zhigang.chen@owulia.com>
2019-02-19 17:23:39 +0800

    提交更新也买年数据

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,19 +1,1 @@
 
-$(() => {
-    var $navItems = $('.nav-item');
-    var $navInnerItems = $('.nav-item-inner');
-    var $mask = $('.mask');
-    $navInnerItems.on('click', function () {
-        var $father = $(this).parent('.nav-item');
-        var is = $father.hasClass('on');
-        $navItems.removeClass('active on');
-        $father.addClass('active');
-        if (is) {
-            $father.removeClass('on');
-            $mask.hide();
-        } else {
-            $father.addClass('on');
-            $mask.show();
-        }
-    })
-});
